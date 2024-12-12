4967b081b8c801fda0047550df1fa00698532733
Thenjiwe <thenjiwelamani15@gmail.com>
2024-12-12 14:04:29 +0200

    cart

--- images/main.js
+++ images/main.js
@@ -2,62 +2,6 @@ let shop = document.getElementById("shop");
 
 let cart = JSON.parse(localStorage.getItem("data")) || [];
 
-let shopItemsData = [
-{
-    id:"item1",
-    name:"Abstract Hoop",
-    price: 55,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "img-1.jpg"
-},
-{
-    id:"item2",
-    name:"Bold Square Earings",
-    price: 45,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "img-2.jpg"
-},
-{
-    id:"item3",
-    name:"Ear Stack Set",
-    price: 50,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "img-3.jpg"
-},
-{
-    id:"item4",
-    name:"Spiral Hoops",
-    price: 45,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "img-4.jpg"
-},
-{
-    id:"item5",
-    name:"Ovals Necklace",
-    price: 65,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "img-5.jpg"
-},
-{
-    id:"item6",
-    name:"Ring Stack Set",
-    price: 40,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "img-6.jpg"
-},
-{
-    id:"item7",
-    name:"Spine Necklace",
-    price: 95,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "img-7.jpg"
-},
-{
-    id:"item8",
-    name:"Simple Necklace and Bracelet Set",
-    price: 80,
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "img-8.jpg"},]
 
 let generateShop =()=>{
     return (shop.innerHTML = shopItemsData.map((x)=>{
