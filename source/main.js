1b023e6d40bccc57d5e62ef4dd7c5276967454c4
Thenjiwe <thenjiwelamani15@gmail.com>
2025-01-04 15:48:49 +0200

    moved files

--- source/main.js
+++ source/main.js
@@ -139,7 +139,7 @@ let decrement = (id) => {
   }
   update(selectedItem.id);
   basket = basket.filter((x) => x.item !== 0);
-  // console.log(basket);
+  
   localStorage.setItem("data", JSON.stringify(basket));
 };
 let update = (id) => {
